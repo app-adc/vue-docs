73b1fa6e3ceb55bc19b6101491e2de1932c4f5e3
pure cc <pure@autopair.co>
2024-12-05 17:01:21 +0700

    set base path for Vite configuration to '/BcDocs'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-
+    base: '/BcDocs',
     server: {
         port: 3000,
         watch: {
