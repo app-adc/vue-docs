97d2ba9f4805494480c486499c06eb430a2f8198
pure cc <pure@autopair.co>
2024-12-05 17:22:48 +0700

    add home route configuration for the application

---
+++ src/router/homeRoute.ts
@@ -0,0 +1,15 @@
+import { AppTitle } from '@/store/app'
+import { RouteRecordRaw } from 'vue-router'
+
+const general: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/ABC/pages/bc_doc_http.vue'),
+        meta: {
+            title: `${AppTitle} | Home`,
+        },
+    },
+]
+
+export default general
